21d0a75561a2222ed1d7066e75e53d80a1b163c5
Abdulmalik Felemban <159801953+CookieMadness11243@users.noreply.github.com>
2026-07-24 15:33:07 +0300

    Refactor WordGuesser.py with improved comments

--- WordGuesser.py
+++ WordGuesser.py
@@ -7,32 +7,38 @@
 6- Continue until the word is guessed or all attempts are exhausted.
 7- Display the result.
 """
-
+# Hello It's me! Abdulmalik.
 word = "advanced"
 word = list(word)
 
 guesses = 0
 chances = 7
 
+# Creating a second list with the name if word2, the array I'll work with to print out the guesses and answers
 word2 = word.copy()
 for replace in range(len(word)):
     word2[replace] = "_"
 print(f"Guess The Word! {" ".join(word2)}")
 
+# Game Loop
 while guesses != chances:
+    # Win Case:
     if word2 == word:
         print(f"you won! the word is {"".join(word)}")
         break
     guess_letter = str(input(f"What's your guess? you have {chances} chances: "))
-
+    
+    # Guessing the right letter
     if guess_letter in word:
         for CorrectGuess in range(len(word)):
             if guess_letter == word[CorrectGuess]:
                 word2[CorrectGuess] = word[CorrectGuess]
         print("Correct!", " ".join(word2))
+        # Losing all chances
     elif guesses >= chances:
         print("You Loes :(")
         break
+        # Guessing wrong
     else:
         guesses+=1
         print(f"Wrong, Guesses left {chances - guesses}")
