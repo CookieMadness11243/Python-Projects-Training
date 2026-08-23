c8c60b236c32c515473dfdedc546860ed922c1c9
beepas-coder <ianbeepas@gmail.com>
2026-08-23 11:09:46 +0300

    Refactor WordGuesser.py for clarity and fixes
    Refactor WordGuesser game logic and improve readability.
    
    All changes i comment in code.
    
    1. Turned word in to list in one line of code (originally two lines)
    2. Used List Comprehension to word2
    3. Fixed a bug on line 26, but i don't remember what it was

--- WordGuesser/WordGuesser.py
+++ WordGuesser/WordGuesser.py
@@ -8,22 +8,21 @@
 7- Display the result.
 """
 
-word = "advanced"
-word = list(word)
+word = list("advanced")  # make a list of letters in place
 
 guesses = 0
 chances = 7
 
-word2 = word.copy()
-for replace in range(len(word)):
-    word2[replace] = "_"
-print(f"Guess The Word! {" ".join(word2)}")
+word2 = ["_" for letter in word]  # used List Comprehension (in case you didn't know that thing existed, there a good documentation here => https://labex.io/pythoncheatsheet/cheatsheet/comprehensions)
+
+print(f"Guess The Word! {" ".join(word2)}") 
 
 while guesses != chances:
+    print()  # for better redability
     if word2 == word:
         print(f"you won! the word is {"".join(word)}")
         break
-    guess_letter = str(input(f"What's your guess? you have {chances} chances: "))
+    guess_letter = str(input(f"What's your guess? you have {chances-guesses} chances: "))  # fixed a bug
 
     if guess_letter in word:
         for CorrectGuess in range(len(word)):
@@ -31,8 +30,8 @@ while guesses != chances:
                 word2[CorrectGuess] = word[CorrectGuess]
         print("Correct!", " ".join(word2))
     elif guesses >= chances:
-        print("You Loes :(")
+        print("You Loes :(")  # does'nt show when you lose
         break
     else:
         guesses+=1
-        print(f"Wrong, Guesses left {chances - guesses}")
+        print(f"Wrong, Guesses left {chances - guesses}")  # kinda useless because you already wrote guesses left in line 26
